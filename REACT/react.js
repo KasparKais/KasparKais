342fbe16eb346e5fb4321084aba598bbf3254425
Kaspar <kasparkais@gmail.com>
2020-03-15 17:00:00 +0000

    REACT basics

---
+++ REACT/react.js
@@ -0,0 +1,36 @@
+(function() {
+    class MyHTMLComponent extends React.Component {
+
+        constructor(props) {
+            super(props);
+            this.count = 0;
+            this.addOneOne = this.addOne.bind(this);
+        }
+
+        addOne() {
+            this.setState(() => this.count ++);
+        }
+
+        render() {
+            return(
+                <div>
+                    <h1>Test title</h1>
+                    <button>Wooop</button>
+                    <h2>{this.props.extraText}</h2>
+
+                    <button onClick={this.addOneOne}>Click to add one to the sum</button>
+                    Count: {this.count}
+                </div> 
+                
+            );
+        }
+    }
+
+    ReactDOM.render(<MyHTMLComponent extraText="Some text added with attribute"></MyHTMLComponent>, document.getElementById('container'));
+})();
+
+// augstāk mēs izsaucam funkciju un uzreiz definējam. Īsakais veidz šim
+
+// function testFn() {
+//  }
+// testFn()
